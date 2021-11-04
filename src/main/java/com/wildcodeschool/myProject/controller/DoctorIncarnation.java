15bea0e3c426a357244fa62a77e36a84b1894713
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2021-11-04 12:41:03 +0100

    Spring_quest02_updated

--- src/main/java/com/wildcodeschool/myProject/controller/DoctorIncarnation.java
+++ src/main/java/com/wildcodeschool/myProject/controller/DoctorIncarnation.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
@@ -13,11 +14,12 @@ public class DoctorIncarnation {
     @RequestMapping("/doctor/incarnation/{number}")
     @ResponseBody
     public Doctor index(@PathVariable int number) {
+
         if (number > 0 && number < 9) {
             // exception 303
             throw new ResponseStatusException(HttpStatus.SEE_OTHER);
         }
-
+        //try {
         switch (number) {
             case 9:
                 Doctor doc9 = new Doctor(9, "Christopher Eccleston");
@@ -36,7 +38,7 @@ public class DoctorIncarnation {
                 return doc13;
             default:
                 // exception 404
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible to retrieve the incarnation");
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible to retrieve the incarnation " + number);
         }
     }
 }
